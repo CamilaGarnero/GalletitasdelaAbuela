7fcd6020771a1146c7ec489a3894756dac290256
Camila Garnero <camilagarnero10@gmail.com>
2022-04-14 21:41:35 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
+  const styles = {
+    header:"text-3xl font-bold text-yellow-600"
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Bienvenidos a Las Galletitas de la Abuela</h1>
-      </header>
+      <NavBar/>
+      <h1 className={styles.header}>Bienvenidos a la cocina de la Nona</h1>
+      {/*<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="http://reactjs.ong"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+  </header>*/ }
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const NavBar = () => {
+    const styles = {
+        logo:"btn btn-ghost normal-case text-xl text-black hover:text-amber-400",
+        item:"text-black hover:border-amber-300 hover:bg-emerald-600",
+        subtienda:"p-2 bg-emerald-600"
+    }
+
+    return (
+        <div className='navbar bg-emerald-300/75'>
+            <div className='flex-1'>
+                <a className={styles.logo}>Las Galletitas de la Abuela</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal p-0">
+                    <li className={styles.item}><a>Nosotros</a></li>
+                    <li tabindex="0">
+                        <a className='text-black'>
+                        Tienda
+                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                        </a>
+                        <ul className={styles.subtienda}>
+                            <li><a>Submenu 1</a></li>
+                            <li><a>Submenu 2</a></li>
+                        </ul>
+                    </li>
+                    <li className={styles.item}><a>Carrito</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
